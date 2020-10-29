e37c8cb0a323d5f9e53fa9ff7727aacb2d9429d2
Alan Antonio Ruiseco Cisneros <alanantonio.ruisecocisneros@tyson.com>
2020-10-29 08:27:49 -0600

    update

--- gitlab-101/get-started/background.sh
+++ gitlab-101/get-started/background.sh
@@ -1,5 +1,7 @@
 mkdir /home/scrapbook/tutorial/git/my-remote/project
 
+mkdir /home/scrapbook/tutorial/git/my-local/
+
 cd /home/scrapbook/tutorial/git/my-remote/project && git --bare init
 
 cd /home/scrapbook/tutorial/git/my-local/
