2ec9502471ab6f5f153f943686b7d4494a0a937a
Alan Antonio Ruiseco Cisneros <alanantonio.ruisecocisneros@tyson.com>
2020-10-29 15:41:01 -0600

    Added git config

--- gitlab-101/get-started/background-step1.sh
+++ gitlab-101/get-started/background-step1.sh
@@ -1,8 +1,2 @@
-mkdir -p /home/scrapbook/tutorial/git/my-remote/project
-mkdir -p /home/scrapbook/tutorial/git/my-local/
-
-cd /home/scrapbook/tutorial/git/my-remote/project
-
-git --bare init
 git config --global user.email "dev_one@example.com"
 git config --global user.name "Dev One"
