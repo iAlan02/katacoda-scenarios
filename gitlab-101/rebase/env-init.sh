8f8ac6f27b48a45ebe28e1e7b981b5bd542ca4ef
Alan Antonio Ruiseco Cisneros <alanantonio.ruisecocisneros@tyson.com>
2020-11-06 07:12:08 -0600

    Updated

---
+++ gitlab-101/rebase/env-init.sh
@@ -0,0 +1,50 @@
+apt-get --yes update
+
+apt-get --yes install ruby
+
+mkdir -p /home/scrapbook/tutorial/git/my-remote/project
+
+mkdir -p /home/scrapbook/tutorial/git/my-local/
+
+cd /home/scrapbook/tutorial/git/my-remote/project && git --bare init
+
+git config --global user.email "dev_one@example.com"
+git config --global user.name "Dev One"
+
+cd /home/scrapbook/tutorial/git/my-local/
+
+git clone /home/scrapbook/tutorial/git/my-remote/project
+
+cd project
+
+git config --global user.email "dev_one@example.com"
+git config --global user.name "Dev One"
+
+touch new-file
+
+git add . 
+git commit -m "Adding in a test file"
+git push origin master
+git checkout -b add-file-2
+
+touch new-file-2
+
+git add .
+git commit -m "Adding in another new file"
+git push origin add-file-2
+git checkout master
+git merge add-file-2
+git push origin master
+git checkout add-file-2
+git checkout -b add-hello-world
+
+cat << EOF >> hello.rb 
+puts "Hello, World!"
+EOF
+
+git add hello.rb
+git commit -m 'adding in hello world script'
+git push origin add-hello-world
+git checkout master
+git merge add-hello-world
+git push origin master
---
+++ gitlab-101/rebase/step1.md
@@ -0,0 +1,5 @@
+# To avoid merge commits and the noise they create, we’re going to change a project setting to enforce fast-forward merges. This setting **ensures every feature branch being merged into master is a proper superset of master**, meaning that a feature branch contains all of the current work/history of master before merging is allowed
+
+## Tasks
+
+Let’s make sure we’re up to date on local master with origin/master with a pull.  
