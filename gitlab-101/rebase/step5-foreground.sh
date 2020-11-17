0579bfc35f72bf419a0004954cc866409aff5d50
ruca93 <alan_antoniorc@outlook.com>
2020-11-17 15:12:02 -0600

    updated

--- gitlab-101/rebase/step5-foreground.sh
+++ gitlab-101/rebase/step5-foreground.sh
@@ -1,6 +1,6 @@
-cd /home/scrapbook/tutorial/git/my-local-1
+cd /home/scrapbook/tutorial/git/my-local-1/project
 git checkout master
 git merge add-another-script
 git push origin master
 git checkout add-another-script
-cd /home/scrapbook/tutorial/git/my-local-2
+cd /home/scrapbook/tutorial/git/my-local-2/project
