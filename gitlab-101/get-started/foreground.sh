44e01e3cb65f696eb5e414bcb4f999b05cbef8ab
Ruiseco <acisneros@expediagroup.com>
2021-10-28 17:17:48 -0500

    Test foreground

--- gitlab-101/get-started/foreground.sh
+++ gitlab-101/get-started/foreground.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 add-apt-repository ppa:git-core/ppa
+
 apt-get --yes update
+
 apt-get --yes install git
 
 mkdir -p /home/scrapbook/tutorial/git/my-remote/project
@@ -10,5 +12,7 @@ mkdir -p /home/scrapbook/tutorial/git/my-local/
 cd /home/scrapbook/tutorial/git/my-remote/project
 
 git --bare init
+
 git config --global user.email "dev_one@example.com"
+
 git config --global user.name "Dev One"
