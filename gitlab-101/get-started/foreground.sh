0edc3c81d78f039e033f6f20c522a3fd81e824e1
Ruiseco <acisneros@expediagroup.com>
2021-10-28 12:19:37 -0500

    Fixing env init

---
+++ gitlab-101/get-started/foreground.sh
@@ -0,0 +1,12 @@
+add-apt-repository ppa:git-core/ppa
+apt-get --yes update
+apt-get --yes install git
+
+mkdir -p /home/scrapbook/tutorial/git/my-remote/project
+mkdir -p /home/scrapbook/tutorial/git/my-local/
+
+cd /home/scrapbook/tutorial/git/my-remote/project
+
+git --bare init
+git config --global user.email "dev_one@example.com"
+git config --global user.name "Dev One"
