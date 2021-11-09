c7211da4ce14df110b10f5b458a3ade498f0a994
Ruiseco <acisneros@expediagroup.com>
2021-11-09 16:05:09 -0600

    Removing bare

--- gitlab-101/branch-and-merge/env-init.sh
+++ gitlab-101/branch-and-merge/env-init.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-apt-get --yes install ruby
-
 mkdir -p /home/scrapbook/tutorial/git/my-remote/project
 mkdir -p /home/scrapbook/tutorial/git/my-local/
 
-cd /home/scrapbook/tutorial/git/my-remote/project && git --bare init
+cd /home/scrapbook/tutorial/git/my-remote/project && git init
 
 git config --global user.email "dev_one@example.com"
 git config --global user.name "Dev One"
--- gitlab-101/branch-and-merge/step3-foreground.sh
+++ gitlab-101/branch-and-merge/step3-foreground.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd /home/scrapbook/tutorial/git/my-remote/project
+echo "initializing step"
 
-git checkout master
-git merge add-file-2
-git push origin master
-git checkout add-file-2
+sleep 10
