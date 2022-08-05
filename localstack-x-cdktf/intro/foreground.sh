30c3eb8fdadf87e0c9784f30a8209de8aab6ff78
Ruiseco <acisneros@expediagroup.com>
2022-08-05 15:26:43 -0500

    Fixed wrong syntax

--- localstack-x-cdktf/intro/foreground.sh
+++ localstack-x-cdktf/intro/foreground.sh
@@ -1,5 +1,5 @@
 echo "await until NodeJS and Terraform gets installed"
-apt install --yes nodejs npm && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install 16 && clear
+apt install --yes nodejs npm && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install 16 && clear
 # apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 # curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 # sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
