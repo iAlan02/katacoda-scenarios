4f6182f7b879e5684380d7e7abc3ef972d10fb50
Ruiseco <acisneros@expediagroup.com>
2023-05-31 10:20:06 -0600

    updated nvm to install latest node version

--- localstack-x-cdktf/intro/intro.sh
+++ localstack-x-cdktf/intro/intro.sh
@@ -1,2 +1,2 @@
 echo "await until NodeJS"
-apt install --yes nodejs npm && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install 16 && clear && echo "NodeJS installed"
+apt install --yes nodejs npm && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install --lts && clear && echo "NodeJS installed"
